378eec8c4a3b0089d26781d9fc769aa5c20611c3
Gašper <gasper.pistotnik@gmail.com>
2025-04-13 03:23:26 +0200

    darkmode

--- app/index.tsx
+++ app/index.tsx
@@ -55,11 +55,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
   },
   content: {
     flex: 1,
     padding: 16,
+    backgroundColor: '#000000',
   },
   controls: {
     marginTop: 16,
